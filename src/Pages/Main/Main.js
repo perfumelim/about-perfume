bac08a11e19dc77659536f2be6a39f26b57439b0
perfumellim <perfumelim@gmail.com>
2021-01-10 21:17:51 +0900

    merge를 위한 commit

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,14 +1,10 @@
 import React from "react";
+import stamp from "./stamp.JPG";
 import { withRouter } from "react-router-dom";
 import "./Main.scss";
 
 const Main = () => {
-  return (
-    <div className="showcase">
-      <h1>DEVSISTERS</h1>
-      <p>CREATING THE BEST PLAYER EXPERIENCES</p>
-    </div>
-  );
+  return <div className="showcase"></div>;
 };
 
 export default withRouter(Main);
