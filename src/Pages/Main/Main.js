aecdf8a29b435654e8f53b18e0b2dd210a971586
perfumellim <perfumelim@gmail.com>
2021-01-10 22:04:05 +0900

    Add: 레이아웃 구성

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,10 +1,24 @@
 import React from "react";
 import stamp from "./stamp.JPG";
+import { GrGithub, GrLanguage, GrMail } from "react-icons/gr";
 import { withRouter } from "react-router-dom";
 import "./Main.scss";
 
 const Main = () => {
-  return <div className="showcase"></div>;
+  return (
+    <div className="showcase">
+      <img alt="main_image" src={stamp} width="100%" />
+      <div className="introduce">
+        <span>Hello,</span>
+        <p>I'm Perfume!</p>
+        <div className="iconZone">
+          <GrGithub />
+          <GrLanguage />
+          <GrMail />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default withRouter(Main);
