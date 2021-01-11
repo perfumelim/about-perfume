22a3c270d5cb3c07417c12829055879dfd7e3810
perfumellim <perfumelim@gmail.com>
2021-01-11 22:40:55 +0900

    Add: Mock data 추가

---
+++ src/Pages/Shop/Shop.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+import "./Shop.scss";
+
+const Shop = () => {
+  return <div className="shopPages"></div>;
+};
+
+export default withRouter(Shop);
